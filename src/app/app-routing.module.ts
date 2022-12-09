760ec141749f9cdba4698d2c64a316f18878d81f
Mahmoud Shata <mahmoudhosam23@gmail.com>
2022-12-09 15:35:25 +0200

    starting project & created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '404',
+    component: ProductsComponent ,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: ProductsComponent,
+    data: {
+      title: 'Page 500'
+    }
+  }, {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  }, {
+    path: '',
+    component: ProductsComponent,
+    children: [
+      {
+        path: '',
+        component: ProductsComponent,
+        // canActivate: [AuthGuard],
+        data: {
+          breadcrumb: [
+            {
+              label: 'Home',// pageOneID Parameter value will be add
+              url: './'
+            }
+          ],
+        }
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
